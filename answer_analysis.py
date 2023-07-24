7d7e704e1983b8e95b346dafe2ca949f495fe8ea
RocioBLion <rociosabatins12@gmail.com>
2023-07-23 22:06:06 -0300

    Grafico las metricas

--- answer_analysis.py
+++ answer_analysis.py
@@ -1,13 +1,28 @@
 import pandas as pd
 from gensim.models import KeyedVectors
 import numpy as np
+import matplotlib.pyplot as plt
 
-analogias_df = pd.read_csv('analogies_answers.csv')
+# ruta de la laptop
+#analogias_df = pd.read_csv('analogies_answers.csv')
 
-#embeddings_WIKI = KeyedVectors.load_word2vec_format('embeddings/wiki.es.vec', limit=None)
-embeddings_SUC = KeyedVectors.load_word2vec_format('embeddings/embeddings-l-model.vec', limit=None)
-embeddings_SWOW = KeyedVectors.load_word2vec_format('embeddings/swow.embedding.was.26-04-2022.vec', limit=None)
+analogias_df = pd.read_csv('C:\\Users\\bruno\\Desktop\\REPO CABANA\\WordEmbeddings-Analogias\\analogies_answers.csv')
+
+#embeddings_WIKI = KeyedVectors.load_word2vec_format('WordEmbeddings-Analogias/embeddings/wiki.es.vec', limit=None)
+embeddings_SUC = KeyedVectors.load_word2vec_format('WordEmbeddings-Analogias/embeddings/embeddings-l-model.vec', limit=None)
+embeddings_SWOW = KeyedVectors.load_word2vec_format('WordEmbeddings-Analogias/embeddings/swow.embedding.was.26-04-2022.vec', limit=None)
 lower_embeddings_SWOW = KeyedVectors(vector_size=embeddings_SWOW.vector_size)
+
+#estos no cargan
+#embeddings_SBWC = KeyedVectors.load_word2vec_format('WordEmbeddings-Analogias/embeddings/SBW-vectors-300-min5.txt', limit=None)
+#embeddings_Glove = KeyedVectors.load_word2vec_format('WordEmbeddings-Analogias/embeddings/glove-sbwc.i25.vec', limit=None)
+
+
+# Rutas de la laptop
+#embeddings_WIKI = KeyedVectors.load_word2vec_format('embeddings/wiki.es.vec', limit=None)
+#embeddings_SUC = KeyedVectors.load_word2vec_format('embeddings/embeddings-l-model.vec', limit=None)
+#embeddings_SWOW = KeyedVectors.load_word2vec_format('embeddings/swow.embedding.was.26-04-2022.vec', limit=None)
+#lower_embeddings_SWOW = KeyedVectors(vector_size=embeddings_SWOW.vector_size)
 #embeddings_SBWC = KeyedVectors.load_word2vec_format('embeddings/SBW-vectors-300-min5.txt', limit=None)
 #embeddings_Glove = KeyedVectors.load_word2vec_format('embeddings/glove-sbwc.i25.vec', limit=None)
 
@@ -82,3 +97,37 @@ topK_metricas_todas = [calcular_topK_metricas(prediccion, analogias_df) for pred
 print("Todas las metricas topK: ", topK_metricas_todas)
 
 
+#top 10: [0.33783783783783783, 0.36486486486486486]
+#top 5: [0.2702702702702703, 0.3108108108108108]
+#top 100: [0.5405405405405406, 0.6351351351351351]
+
+# Graficas
+
+modelos = ["embeddings_SUC", "lower_embeddings_SWOW"]
+
+metricas_top10 = [0.33783783783783783, 0.36486486486486486]
+metricas_top5 = [0.2702702702702703, 0.3108108108108108]
+metricas_top100 = [0.5405405405405406, 0.6351351351351351]
+
+df = pd.DataFrame({"Modelo": modelos, "Metrica de Rendimiento": metricas_top10})
+
+#Grafica top10
+
+plt.bar(modelos, metricas_top10)
+plt.xlabel('Modelos')
+plt.ylabel('Métricas de rendimiento top 10')
+plt.show()
+
+#Grafica top5
+
+plt.bar(modelos, metricas_top5)
+plt.xlabel('Modelos')
+plt.ylabel('Métricas de rendimiento top 5')
+plt.show()
+
+#Grafica top100
+
+plt.bar(modelos, metricas_top100)
+plt.xlabel('Modelos')
+plt.ylabel('Métricas de rendimiento top 100')
+plt.show()
