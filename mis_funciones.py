2eb9f7a749dbb325c27b22d4c2abb128d7879d93
Álvaro Cabana <user.name>
2023-11-26 21:36:34 -0300

    agregué el K variables en la metrica top_K

--- mis_funciones.py
+++ mis_funciones.py
@@ -17,11 +17,11 @@ def calcular_predicciones(embedding, analogias_df, K_fijo):
             palabras_no_encontradas.append(str(e).split("'")[1]) # se agrega la palabra no encontrada a la lista palabras_no_encontradas
     return predicciones  # devuelve la lista de predicciones.
 
-def metricas_topK(predicciones, analogias_df):
+def metricas_topK(predicciones, analogias_df,K):
     analogias_df['response'] = analogias_df['response'].fillna('') # se llenan los valores nulos en la columna 'response'
     analogias_df['response'] = analogias_df['response'].apply(lambda x: str(x)) # chequeamos de que todos los valores sean strings
     topK_metricas = [] # lista que va a almacenar las métricas calculadas 
     for i, prediccion in enumerate(predicciones): # bucle que itera sobre las predicciones 
         respuesta_humana = analogias_df.iloc[i]['response'].lower() # se obtiene la respuesta humana para la analogía actual y se la convierte a minúsculas
-        topK_metricas.append(int(respuesta_humana in prediccion)) # se agrega 1 a la lista si la respuesta humana está en las predicciones, y 0 en caso contrario 
+        topK_metricas.append(int(respuesta_humana in prediccion[0:K])) # se agrega 1 a la lista si la respuesta humana está en las predicciones, y 0 en caso contrario 
     return np.mean(topK_metricas) # devuelve el promedio de la lista topK_metricas, lo que representa la métrica final de precisión top-K 
